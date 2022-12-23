e89c9300e557f20286ec1b6f8feb78f8f9e59b2a
AlexanderSch12 <alexander.schoeters@gmail.com>
2022-12-23 15:07:07 +0100

    depth 5 + new sorting -eval score + always ordering

--- Engine.cpp
+++ Engine.cpp
@@ -169,7 +169,7 @@ int Engine_::negamax(Board &board, int depth, int alpha, int beta, PrincipalVari
         } else return 0;
     }
 
-    if(depth == 5 || depth == 4) orderMoves(legalMoves,board);
+    orderMoves(legalMoves,board);
 
     auto pv_buf = PrincipalVariation();
     for (Move &move: legalMoves)
